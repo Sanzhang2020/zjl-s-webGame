885ac4b85bfc047fb6c1da76a6f48b6c6854779e
Gary Zhang <zhangjiale2100@163.com>
2022-03-04 14:22:02 +0800

    playground mark 1

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -61,12 +61,12 @@ class zsGameMenu {
     }
     start() {
     }
-}class zsGame {
+}export class zsGame {
     //构造函数
     constructor(id) {
         this.id = id;
         //找到主对象的那个div即 zs_game
-        this.$zs_game = $('#' + id); 
+        this.$zs_game = $('#' + id);
         this.menu = new zsGameMenu(this);
         this.playground = new zsGamePlayground(this);
         this.start();
--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,9 +1,9 @@
-class zsGame {
+export class zsGame {
     //构造函数
     constructor(id) {
         this.id = id;
         //找到主对象的那个div即 zs_game
-        this.$zs_game = $('#' + id); 
+        this.$zs_game = $('#' + id);
         this.menu = new zsGameMenu(this);
         this.playground = new zsGamePlayground(this);
         this.start();
